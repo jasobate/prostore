d9812003a35fb4704b4f1afb2489ecbb79f9bed0
codestackr <j.bateman1@bigpond.com>
2025-02-07 14:32:49 +1100

    2.32pm-07/02/2025

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,8 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from '@/db/prisma';
+import { formatError } from "../utils";
+
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, 
@@ -67,7 +69,6 @@ export async function signInWithCredentials(prevState: unknown,
         throw error;
       }
 
-      return { success: false, message: 'User was not registered' };
+      return { success: false, message: formatError(error) };
     }
-    
   }
