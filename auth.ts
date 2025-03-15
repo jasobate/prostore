31379b9b1f84a4a7350aaf53cd0887fc06dcfbac
codestackr <j.bateman1@bigpond.com>
2025-03-15 12:35:18 +1100

    Brad Vercel fix

--- auth.ts
+++ auth.ts
@@ -1,10 +1,11 @@
+import { compareSync } from 'bcrypt-ts-edge';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/db/prisma';
 import { cookies } from 'next/headers';
-import { compare } from './lib/encrypt';
+// import { compare } from './lib/encrypt';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const config = {
@@ -35,7 +36,7 @@ export const config = {
 
         // Check if user exists and if the password matches
         if (user && user.password) {
-          const isMatch = await compare(
+          const isMatch = await compareSync(
             credentials.password as string,
             user.password
           );
